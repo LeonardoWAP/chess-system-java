26d6a9d88628f37f95d84d7b9acdf1e43eb2d517
LeonardoWAP <leonardowillersap@gmail.com>
2022-12-21 15:26:36 -0300

    boardgame created

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[]args){
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
